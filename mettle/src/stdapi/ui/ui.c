e701cc9197c3c0a4153c83f944ad915ca537ee3d
Tim <timrlw@gmail.com>
2017-03-30 21:57:34 +0800

    HAVE_SCREENSHOT

--- mettle/src/stdapi/ui/ui.c
+++ mettle/src/stdapi/ui/ui.c
@@ -16,20 +16,16 @@
 #include "log.h"
 #include "tlv.h"
 
-#if __APPLE__
-#include <TargetConditionals.h>
-#endif
-
-#if TARGET_OS_OSX
-extern struct tlv_packet *desktop_screenshot(struct tlv_handler_ctx *ctx);
+#if HAVE_SCREENSHOT
+extern struct tlv_packet *stdapi_ui_desktop_screenshot(struct tlv_handler_ctx *ctx);
 #endif
 
 void ui_register_handlers(struct mettle *m)
 {
 	struct tlv_dispatcher *td = mettle_get_tlv_dispatcher(m);
 
-#if TARGET_OS_OSX
-	tlv_dispatcher_add_handler(td, "stdapi_ui_desktop_screenshot", desktop_screenshot, m);
+#if HAVE_SCREENSHOT
+	tlv_dispatcher_add_handler(td, "stdapi_ui_desktop_screenshot", stdapi_ui_desktop_screenshot, m);
 #endif
 }
 
